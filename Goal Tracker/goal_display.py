53f604eb32d54a58562afe64bd95a6f9af74d4db
Ben White <71787942+BAWhite92@users.noreply.github.com>
2020-11-08 16:14:01 +0000

    Add files via upload
    Updated set_goal to check if already exists, added update goal progress, refactored checking if goal exists, see_progress now checks if goal exists. Fixed issue with code occasionally writing same goal twice in .txt file when updating a goal.

--- Goal Tracker/goal_display.py
+++ Goal Tracker/goal_display.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt 
+import re
 
 class GoalTracker:
     """Class to show current progress for goals"""
@@ -7,16 +8,43 @@ class GoalTracker:
         self.doc = "user_goals.txt"
     
     def set_goal(self, goal_name):
-        with open(self.doc, "a+") as goal_doc:
-            goal_doc.write(f"\n{goal_name}: 0")
+        #set a new goal to be tracked
+        if self.check_goal_exists(goal_name) == False:
+            with open(self.doc, "a+") as goal_doc:
+                goal_doc.write(f"{goal_name}: 0\n")
+        else:
+            print("You already have this goal!")
 
+    def update_goal_progress(self, goal_name, progress):
+        #update an existing goal, only progress to be altered
+        if self.check_goal_exists(goal_name):
+            with open(self.doc, "r") as txt:
+                lines = txt.readlines()
+                with open(self.doc, "w+") as txt:
+                    for goal in lines:
+                        if goal_name not in goal:
+                            txt.write(goal)
+                    txt.write(f"{goal_name}: {progress}\n")
+        else:
+            print("No such Goal Exists!")
+
+    def check_goal_exists(self, goal_name):
+        #check if a goal exists already
+        with open(self.doc, "r") as txt:
+            lines = txt.readlines()
+            test_for_goal = ' '.join([line for line in lines])
+            if re.findall("%s" % goal_name, test_for_goal):
+                return True
+            else:
+                return False
 
     def see_progress(self, goal):
+        present = False
         with open(self.doc) as goal_doc:
             for line in goal_doc:
-                print("line")
                 goal_name = line.split(":")
-                if goal in goal_name:
+                if goal == goal_name[0]:
+                    present = True
                     completed = int(goal_name[1])
                     if completed > 0:
                         labels = "Progression", "Remaining"
@@ -26,6 +54,8 @@ class GoalTracker:
                     title = goal_name[0]
                     pie = self.create_pie_chart(labels, sizes, title)
                     plt.show() 
+            if present == False:
+                print("No such Goal, please try again")
 
     def create_pie_chart(self, labels, sizes, title):
         fig1, ax = plt.subplots()
@@ -37,4 +67,7 @@ class GoalTracker:
 ben = GoalTracker()
 str = "Upload GitHub Repository"
 ben.see_progress(str)
-#ben.set_goal("Upload GitHub Repository")
+ben.set_goal(str)
+ben.see_progress(str)
+ben.update_goal_progress("Upload GitHub Repository", 35)
+ben.see_progress(str)
